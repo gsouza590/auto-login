3d6471cff77dad7d68e3c3965a88419363406299
gsouza590 <gsouza590@gmail.com>
2023-07-10 13:49:44 -0300

    fix tela hora personalizada

--- utils/modulo_tela_dia_personalizado.py
+++ utils/modulo_tela_dia_personalizado.py
@@ -3,17 +3,23 @@ import PySimpleGUI as sg
 def dia_personalizacao():
     # Defina o layout da janela de registros
     registro_layout = [
-        [sg.Text('Dia   \t\tEntrada 1    \t\tEntrada 2')]
+        [sg.Text('Dia   \t\tEntrada 1   \t\tSaida 1       \t\tEntrada 2  \t\tSaida 2    \t\tOcorrência Desportiva')]
     ]
 
     for i in range(1, 32):
         registro_layout.append([
             sg.Text(f'{i}\t'),
-            sg.Input(size=(10, 2), key=f'entrada1_hora_{i}'), sg.Text(':'),
-            sg.Input(size=(10, 2), key=f'entrada1_minuto_{i}'),
-            sg.Input(size=(10, 2), key=f'entrada2_hora_{i}'), sg.Text(':'),
-            sg.Input(size=(10, 2), key=f'entrada2_minuto_{i}'),
-            sg.Checkbox('Ocorrência Desportiva', key=f'ocorrencia_desportiva_{i}')
+            sg.Input(size=(8, 2), key=f'entrada1_hora_{i}'), sg.Text(':'),
+            sg.Input(size=(8, 2), key=f'entrada1_minuto_{i}'),
+            sg.Input(size=(8, 2), key=f'saida1_hora_{i}'), sg.Text(':'),
+            sg.Input(size=(8, 2), key=f'saida1_minuto_{i}'),
+            sg.Input(size=(8, 2), key=f'entrada2_hora_{i}'), sg.Text(':'),
+            sg.Input(size=(8, 2), key=f'entrada2_minuto_{i}'),
+            sg.Input(size=(8, 2), key=f'saida2_hora_{i}'), sg.Text(':'),
+            sg.Input(size=(8, 2), key=f'saida2_minuto_{i}'),
+            sg.Checkbox(text="" ,key=f'ocorrencia_desportiva_{i}'),
+            sg.Input(size=(8, 2), key=f'entradaOcorrencia_hora_{i}'), sg.Text(':'),
+            sg.Input(size=(8, 2), key=f'entradaOcorrencia_hora_{i}'),
         ])
 
     # Crie a janela de registros
