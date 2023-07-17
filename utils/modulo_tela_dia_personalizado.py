c870355552421cd4f0455c662873102a5f3664fb
gsouza590 <gsouza590@gmail.com>
2023-07-17 14:13:33 -0300

    Add hora personalizada e ocorrência

--- utils/modulo_tela_dia_personalizado.py
+++ utils/modulo_tela_dia_personalizado.py
@@ -28,9 +28,11 @@ def dia_personalizacao(navegador):
             sg.Input(size=(6, 2), key=f'saida2_hora_{i}', pad=(0, 0)), sg.Text(':', pad=(0, 0)),
             sg.Input(size=(6, 2), key=f'saida2_minuto_{i}', pad=((0, 20), (0, 0))),
 
-            sg.Checkbox(text="", key=f'ocorrencia_desportiva_{i}', pad=((0, 4), (0, 0))),
+            sg.Checkbox('Ocorrência Desportiva', key=f'desporte_{i}', enable_events=True, default=False, pad=((0, 4), (0, 0))),
             sg.Input(size=(6, 2), key=f'entradaOcorrencia_hora_{i}', pad=(0, 0)), sg.Text(':', pad=(0, 0)),
-            sg.Input(size=(6, 2), key=f'entradaOcorrencia_minuto_{i}', pad=(0, 0))
+            sg.Input(size=(6, 2), key=f'entradaOcorrencia_minuto_{i}', pad=(0, 0)),
+            sg.Input(size=(6, 2), key=f'saidaOcorrencia_hora_{i}', pad=(0, 0)), sg.Text(':', pad=(0, 0)),
+            sg.Input(size=(6, 2), key=f'saidaOcorrencia_minuto_{i}', pad=(0, 0))
         ])
     registro_layout.append([sg.Button('Confirmar')])
 
@@ -42,6 +44,7 @@ def dia_personalizacao(navegador):
         event, valores = registro_window.read()
         if event == sg.WINDOW_CLOSED:
             break
+
         if event == 'Confirmar':
             preenchimento_horario_manual(navegador, valores)
 
@@ -50,6 +53,7 @@ def dia_personalizacao(navegador):
 
 def preenchimento_horario_manual(navegador, valores):
     elementos = navegador.find_elements('xpath', "//*[@id='formInclusao:opRelatorioModal:tb']/tr")
+
     for i in range(1, 32):
         time.sleep(3)
         pegaTexto = navegador.find_element('xpath', '//*[@id="formInclusao:opRelatorioModal:0:j_id501"]')
@@ -64,13 +68,25 @@ def preenchimento_horario_manual(navegador, valores):
             time.sleep(3)
             navegador.find_element(By.ID, "formInclusao:opRelatorioModal:0:j_id507").click()
             continue
+
+        checkbox_id = f'desporte_{i}'
+        if valores[checkbox_id]:
+            horaEntrada = valores[f'entrada1_hora_{i}'] + valores[f'entrada1_minuto_{i}']
+            horaSaida = valores[f'saida1_hora_{i}'] + valores[f'saida1_minuto_{i}']
+            horaEntrada2 = valores[f'entrada2_hora_{i}'] + valores[f'entrada2_minuto_{i}']
+            horaSaida2 = valores[f'saida2_hora_{i}'] + valores[f'saida2_minuto_{i}']
+            horaEntradaOcorrencia = valores[f'entradaOcorrencia_hora_{i}'] + valores[f'entradaOcorrencia_minuto_{i}']
+            horaSaidaOcorrencia = valores[f'saidaOcorrencia_hora_{i}'] + valores[f'saidaOcorrencia_minuto_{i}']
+            registra_tabela_manual(navegador, horaEntrada, horaSaida)
+            registra_tabela_manual(navegador, horaEntrada2, horaSaida2)
+            registra_tabela_ocorrencia(navegador, horaEntradaOcorrencia, horaSaidaOcorrencia)
         else:
-                horaEntrada = valores[f'entrada1_hora_{i}'] + valores[f'entrada1_minuto_{i}']
-                horaSaida = valores[f'saida1_hora_{i}'] + valores[f'saida1_minuto_{i}']
-                horaEntrada2 = valores[f'entrada2_hora_{i}'] + valores[f'entrada2_minuto_{i}']
-                horaSaida2 = valores[f'saida2_hora_{i}'] + valores[f'saida2_minuto_{i}']
-                registra_tabela_manual(navegador, horaEntrada, horaSaida)
-                registra_tabela_manual(navegador, horaEntrada2, horaSaida2)
+            horaEntrada = valores[f'entrada1_hora_{i}'] + valores[f'entrada1_minuto_{i}']
+            horaSaida = valores[f'saida1_hora_{i}'] + valores[f'saida1_minuto_{i}']
+            horaEntrada2 = valores[f'entrada2_hora_{i}'] + valores[f'entrada2_minuto_{i}']
+            horaSaida2 = valores[f'saida2_hora_{i}'] + valores[f'saida2_minuto_{i}']
+            registra_tabela_manual(navegador, horaEntrada, horaSaida)
+            registra_tabela_manual(navegador, horaEntrada2, horaSaida2)
 
         time.sleep(3)
         navegador.find_element(By.ID, "formInclusao:opRelatorioModal:0:j_id507").click()
@@ -104,6 +120,25 @@ def registra_tabela_manual(navegador, horaEntrada, horaSaida):
     campo_inicio.send_keys(Keys.TAB)
     campo_fim = navegador.find_element(By.XPATH, '//*[@id="formInclusao:registrosInclusao:0:j_id608"]/input')
     campo_fim.send_keys(horaSaida)
+    time.sleep(3)
+    navegador.find_element(By.ID, "formInclusao:salvar").click()
+    time.sleep(3)
+
+def registra_tabela_ocorrencia(navegador, horaAtividadeIni, horaAtividadeFim):
+    # Selecionar a Ocorrencia Desportiva
+    select_element = navegador.find_element(By.XPATH,
+                                            "//select[@id='formInclusao:j_id582:selectOcorrencia']/option[text()='Prática desportiva']")
+    select_element.click()
+    time.sleep(3)
+    WebDriverWait(navegador, 10).until(
+        EC.visibility_of_element_located((By.ID, 'formInclusao:registrosInclusao:0:j_id606')))
+
+    campo_inicio = navegador.find_element('xpath', '//*[@id="formInclusao:registrosInclusao:0:j_id606"]/input')
+    campo_inicio.send_keys(Keys.BACKSPACE * 4)
+    campo_inicio.send_keys(horaAtividadeIni)
+    campo_inicio.send_keys(Keys.TAB)
+    campo_fim = navegador.find_element('xpath', '//*[@id="formInclusao:registrosInclusao:0:j_id608"]/input')
+    campo_fim.send_keys(horaAtividadeFim)
     time.sleep(3)
     navegador.find_element(By.ID, "formInclusao:salvar").click()
     time.sleep(3)
